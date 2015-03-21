bf00a9d00d2f522108f6ab4804fc5e7671a212ac
Dmitry <diepress@gmail.com>
2015-03-21 15:02:07 +0200

    task done

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,31 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- function(mtrx)
+    {
+      if (!identical(mtrx, x))
+      {
+        print("set new matrix values and reset cache")
+        x <<- mtrx
+        inv <<- NULL
+      }
+      else
+        print("set identical matrix -- cache won't be reseted")
+    }
+    get <- function()
+    {
+      x
+    }
+    setInverse <- function(i)
+    {
+      inv <<- i
+    }
+    getInverse <- function()
+    {
+      inv
+    }
+    list(set=set, get=get, setInverse=setInverse, getInverse=getInverse)
 }
 
 
@@ -12,4 +36,10 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    if (is.null(x$getInverse()))
+    {
+        print("recalc inverse")
+        x$setInverse( solve(x$get()))
+    }
+    x$getInverse()
 }
